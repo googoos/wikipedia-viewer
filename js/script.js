0d2148c42048ecfdcec5901087c37ecbccdb97ff
Gugus A. Sakti <gugussakti@gmail.com>
2016-11-30 14:27:33 +0700

    add search form

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function openSearchForm() {
+  x = '<input type="text" name="search" placeholder="Search.." onkeydown="searchWiki(this)" ><span class="glyphicon glyphicon-remove" onclick="closeSearchForm()"></span>';
+  document.getElementById("searchbox").innerHTML = x;
+}
+
+function closeSearchForm() {
+  x = '<span class="glyphicon glyphicon-search" onclick="openSearchForm()"></span>'
+  document.getElementById("searchbox").innerHTML = x;
+}
+
+function searchWiki(textInput) {
+  if (event.keyCode == 13) {
+    wikiURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&utf8=1&srsearch=" + textInput.value;
+    $("#searchResult").html("<p>" + wikiURL + "</p>");  
+  }
+}
+
+
+// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&utf8=1&srsearch=Albert+Einstein
