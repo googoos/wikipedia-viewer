ab19f1d1b04e4ae721fb8b8841b7bf1eb8791497
Gugus A. Sakti <gugussakti@gmail.com>
2016-12-09 21:37:36 +0700

    show data from json

--- js/script.js
+++ js/script.js
@@ -10,13 +10,22 @@ function closeSearchForm() {
 
 function searchWiki(textInput) {
   if (event.keyCode == 13) {
-    wikiURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&utf8=1&srsearch=" + textInput.value;
+    wikiURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&utf8=1&srsearch=" + textInput.value + "&callback=?";
     $.getJSON(wikiURL, function(json) {
-      $("#searchResult").html(json.query.search[0].timestamp);
+      var html = "";
+      json.query.search.forEach(function(val) {
+        html += "<p>" + val.title + "</p>";
+        html += "<p>" + val.snippet + "</p>";
+        // var keys = Object.keys(val);
+        // html += "<div class = 'cat'>";
+        // keys.forEach(function(key) {
+        //   html += "<strong>" + key + "</strong>: " + val[key] + "<br>";
+        // })
+        // html += "</div><br>";
+    })
+    
+      
+      $("#searchResult").html(html);
     });  
   }
 }
-
-
-// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&continue=-%7C%7C&utf8=1&srsearch=Albert+Einstein
-// $("#searchResult").html("<p>" + wikiURL + "</p>");
